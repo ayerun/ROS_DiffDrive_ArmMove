c551538f69b5e9df29c0b2b4cf4559b5bb42c2d4
Arun Kumar <arunkumar2021@u.northwestern.edu>
2020-11-04 22:59:10 -0600

    created unit test but it fails

---
+++ arm_move/test/test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" Create a unittest node that tests out the add_two service"""
+import unittest
+import rospy
+import rostest
+from geometry_msgs.msg import Pose, Point
+from arm_move.srv import reset, step
+
+class ArmMoveTests(unittest.TestCase):
+    def __init__(self, *args):
+        super(ArmMoveTests, self).__init__(*args)
+        rospy.init_node("test_client")
+
+    def test_step(self):
+        rospy.wait_for_service("reset")
+        Reset = rospy.ServiceProxy("reset", step)
+        rospy.wait_for_service("step")
+        Step = rospy.ServiceProxy("step", step)
+        box = Pose()
+        box.position = Point(x=0.2,y=0.2,z=0.05)
+        box.orientation.w = 1
+        Reset(box_pose=box,clear_waypoints=True)
+        resp = Step(position=Point(x=0.1,y=0,z=-0.1),gripper=False)
+        self.assertEqual(resp.val, -1)
+
+if __name__ == "__main__":
+    import rostest
+    rostest.rosrun('arm_move', "arm_test_node", ArmMoveTests)
